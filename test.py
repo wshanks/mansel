3a8e7ca84972002d4c790ced0e508fe83663e796
Will S <wsha.code@gmail.com>
2018-02-11 07:16:56 -0500

    Refactoring

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
+'''File picker class for selecting multiple files.
+
+CheckablefileSystemModel supports selecting multiple files/directories
+from a tree view and also supports creation with a pre-selected set of
+paths.
+'''
 # TODO: keep running tally of selected file size
-# TODO: unchecking item removes tristate on siblings
 from pathlib import Path
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 import os
 import sys
-import argparse
 
 
 def debug_trace():
@@ -16,27 +20,24 @@ def debug_trace():
     set_trace()
 
 
-def parseOpt():
-    parser = argparse.ArgumentParser(
-        description=('Select files and directories below path to be output as '
-                     'a list'))
-    parser.add_argument('--path', '-p', help='Root path',
-                        default='.')
-    parser.add_argument('selection', nargs='*')
-    return parser.parse_args()
-
-
 class PathConflict(Exception):
+    'Exception raised by inconsistent paths in a DirTree'
     pass
 
 
 class DirTree:
+    '''Tree of nested dicts modeling a file system
+
+    "root" property points ot root of tree which is a dict point to
+    other dicts. Endpoints in the tree are represented by empty dicts.
+    '''
     def __init__(self, paths):
         self.root = DirTreeItem()
         for path in paths:
             self.insert(path)
 
     def insert(self, path):
+        'Insert path into the tree'
         path = Path(path)
         parent = self.root
         for index, part in enumerate(path.parts):
@@ -50,6 +51,7 @@ class DirTree:
             parent = parent[part]
 
     def remove(self, path):
+        'Remove path from the tree'
         path = Path(path)
         pos = self.root
         for part in path.parts:
@@ -62,6 +64,13 @@ class DirTree:
             pos = parent
 
     def check(self, path):
+        '''Check if path is in tree
+
+        Return:
+            'leaf': in tree with no elements below
+            'parent': in tree with elements nested below
+            'unselected': not in tree
+        '''
         path = Path(path)
         pos = self.root
         for part in path.parts:
@@ -91,7 +100,18 @@ class DirTreeItem(dict):
 
 
 class CheckableFileSystemModel(QtWidgets.QFileSystemModel):
-    def __init__(self, *args, preselection=None, **kwargs):
+    '''QFileSystemModel subclass supporting multiple selection and preselection
+
+    Internally, CheckableFileSystemModel stores all checked items in
+    the 'selected' preperty. All ancestors are stored in the
+    'ancestors' property.
+    '''
+
+    selectionChanged = QtCore.pyqtSignal(QtCore.QFileInfo, int)
+
+    def __init__(self, *args, preselection=None, track_selection_size=True,
+                 **kwargs):
+        'preselection: list of paths relative to rootPath to pre-select'
         super().__init__(*args, **kwargs)
         self.preselection = DirTree(preselection)
         self.selected = set()
@@ -99,7 +119,18 @@ class CheckableFileSystemModel(QtWidgets.QFileSystemModel):
 
         self.directoryLoaded.connect(self._handle_preselection)
 
+        if track_selection_size:
+            pass
+
     def _handle_preselection(self, path):
+        '''Method for expanding model paths to load pre-selected items
+
+        This method is used as a slot to connect to the directoryLoaded
+        siganl. When a path is loaded, it is passed here where all its
+        children are checked to see if any are related to the
+        pre-selection. Pre-selected patsh are marked as
+        checked. Ancestors of pre-selected paths are loaded.
+        '''
         relpath = Path(path)
         relpath = relpath.relative_to(self.rootPath())
 
@@ -110,7 +141,6 @@ class CheckableFileSystemModel(QtWidgets.QFileSystemModel):
                                          start=self.rootPath())
             status = self.preselection.check(child_path)
             if status == 'leaf':
-                print('leaf', path)
                 self.setData(child,
                              QtCore.Qt.Checked,
                              QtCore.Qt.CheckStateRole)
@@ -121,73 +151,63 @@ class CheckableFileSystemModel(QtWidgets.QFileSystemModel):
                 self.fetchMore(child)
 
     def flags(self, index):
+        'Enable checkboxes'
         return super().flags(index) | QtCore.Qt.ItemIsUserCheckable
 
     def data(self, index, role):
-        if role != QtCore.Qt.CheckStateRole or index.column() != 0:
+        # Override parent class method to handle checkboxes
+        if role == QtCore.Qt.CheckStateRole and index.column() == 0:
+            return self._data(index)
+        else:
             return super().data(index, role)
 
+    def _data(self, index):
+        'Get checkbox status data'
         persistent_index = QtCore.QPersistentModelIndex(index)
         if persistent_index in self.selected:
             return QtCore.Qt.Checked
-        elif persistent_index in self.ancestors:
+        elif (persistent_index in self.ancestors or
+              self._has_checked_ancestor(index)):
             return QtCore.Qt.PartiallyChecked
         else:
-            parent = index.parent()
-            while (parent.isValid() and
-                   self.filePath(parent) != self.rootPath()):
-                if QtCore.QPersistentModelIndex(parent) in self.selected:
-                    return QtCore.Qt.PartiallyChecked
-                parent = parent.parent()
             return QtCore.Qt.Unchecked
 
-    def _data(self, index):
-        return self.data(index, QtCore.Qt.CheckStateRole)
+    def _has_checked_ancestor(self, index):
+        parent = index.parent()
+        while (parent.isValid() and
+                self.filePath(parent) != self.rootPath()):
+            if QtCore.QPersistentModelIndex(parent) in self.selected:
+                return True
+            parent = parent.parent()
+
+        return False
 
     def setData(self, index, value, role):
+        # Override parent class method to handle checkboxes
         if role == QtCore.Qt.CheckStateRole:
             return self._setData(index, value)
         else:
             return super().setData(index, value, role)
 
     def _setData(self, index, value):
+        'Set checkbox status data and update ancestors/descendants'
         if self._data(index) == value:
             return True
 
         self._set_check_state(index, value)
 
         if value == QtCore.Qt.Checked:
-            # Make sure ancestors are partially checked
-            parent = index.parent()
-            while self.filePath(parent) != self.rootPath():
-                self._set_check_state(parent, QtCore.Qt.PartiallyChecked)
-                parent = parent.parent()
-
-            # Make sure no checked descendants
-            queue = [index.child(i, index.column())
-                     for i in range(self.rowCount(index))]
-            while queue:
-                item = queue.pop()
-                self._set_check_state(item, QtCore.Qt.Unchecked)
-                queue.extend(item.child(i, index.column())
-                             for i in range(self.rowCount(item)))
-
+            self._partially_check_ancestors(index)
+            self._uncheck_descendants(index)
         elif value == QtCore.Qt.Unchecked:
-            # Uncheck ancestors with no other descendants
-            parent = index.parent()
-            while self.filePath(parent) != self.rootPath():
-                child_data = (self._data(parent.child(i, index.column()))
-                              for i in range(self.rowCount(parent)))
-                if all(state == QtCore.Qt.Unchecked for state in child_data):
-                    self._set_check_state(parent, QtCore.Qt.Unchecked)
-                else:
-                    break
-                parent = parent.parent()
+            self._uncheck_exclusive_ancestors(index)
 
+        self.selectionChanged.emit(self.fileInfo(index), value)
         self.dataChanged.emit(index, index, [])
         return True
 
     def _set_check_state(self, index, value):
+        'Set checkbox status data with no side effects on other indexes'
         persistent_index = QtCore.QPersistentModelIndex(index)
 
         if value == QtCore.Qt.Checked:
@@ -200,36 +220,62 @@ class CheckableFileSystemModel(QtWidgets.QFileSystemModel):
             self.ancestors.discard(persistent_index)
             self.selected.discard(persistent_index)
 
+    def _partially_check_ancestors(self, index):
+        'Mark all ancestors of index as checked'
+        parent = index.parent()
+        while self.filePath(parent) != self.rootPath():
+            self._set_check_state(parent, QtCore.Qt.PartiallyChecked)
+            parent = parent.parent()
+
+    def _uncheck_descendants(self, index):
+        'Mark all descendants of index as unchecked'
+        queue = [index.child(i, index.column())
+                 for i in range(self.rowCount(index))]
+        while queue:
+            item = queue.pop()
+            self._set_check_state(item, QtCore.Qt.Unchecked)
+            queue.extend(item.child(i, index.column())
+                         for i in range(self.rowCount(item)))
+
+    def _uncheck_exclusive_ancestors(self, index):
+        'Uncheck ancestors of index that have no other checked descendants'
+        parent = index.parent()
+        while self.filePath(parent) != self.rootPath():
+            child_data = (self._data(parent.child(i, index.column()))
+                          for i in range(self.rowCount(parent)))
+            if all(state == QtCore.Qt.Unchecked for state in child_data):
+                self._set_check_state(parent, QtCore.Qt.Unchecked)
+            else:
+                break
+            parent = parent.parent()
+
 
 class Ui_Dialog(QtWidgets.QDialog):
     def __init__(self, args, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
-        self.setObjectName("Dialog")
-        self.resize(600, 500)
-
-        self.llayout = QtWidgets.QVBoxLayout(parent)
 
         self.model = CheckableFileSystemModel(preselection=args.selection)
         self.model.setRootPath(os.path.abspath(args.path))
+        self.model.dataChanged.connect(self.update_view)
 
         self.tree = QtWidgets.QTreeView()
         self.tree.setModel(self.model)
         self.tree.setSortingEnabled(True)
         self.tree.setRootIndex(self.model.index(os.path.abspath(args.path)))
-        self.model.dataChanged.connect(self.update_view)
 
+        self.button = QtWidgets.QPushButton("OK")
+        self.button.clicked.connect(self.print_path)
+
+        layout = QtWidgets.QVBoxLayout(parent)
+        layout.addWidget(self.tree)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+        self.setObjectName("Dialog")
+        self.resize(600, 500)
         self.setWindowTitle('Select files')
         self.tree.resize(400, 480)
         self.tree.setColumnWidth(0, 200)
-
-        self.but = QtWidgets.QPushButton("OK")
-
-        self.llayout.addWidget(self.tree)
-        self.llayout.addWidget(self.but)
-
-        self.setLayout(self.llayout)
-
-        self.but.clicked.connect(self.print_path)
 
         QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+Q"), self, self.close)
         QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+W"), self, self.close)
@@ -243,8 +289,19 @@ class Ui_Dialog(QtWidgets.QDialog):
 
 
 if __name__ == "__main__":
-    args = parseOpt()
-    app = QtWidgets.QApplication(sys.argv)
+    def parse_options():
+        'Parse command line arguments'
+        import argparse
+        parser = argparse.ArgumentParser(
+            description=('Select files and directories below path to be output '
+                         'as a list'))
+        parser.add_argument('--path', '-p', help='Root path',
+                            default='.')
+        parser.add_argument('selection', nargs='*')
+        return parser.parse_args()
+
+    args = parse_options()
+    app = QtWidgets.QApplication([])
     ui = Ui_Dialog(args)
     ui.show()
 
